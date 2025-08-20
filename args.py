d037a5416811a028d7250646b9beb2f03cc43e15
gorwt <50569488+gorwt@users.noreply.github.com>
2025-08-20 15:50:58 +0800

    Add files via upload

---
+++ args.py
@@ -0,0 +1,21 @@
+### CONFIGS ###
+dataset = 'cora'
+
+input_dim = 1433
+hidden1_dim = 256
+hidden2_dim = 64
+
+num_gin_layers = 3
+gin_hidden_dim = 256
+
+mlp_hidden_dim = 256
+output_dim = 16
+
+use_feature = True
+
+dropout = 0.5
+
+gating_lambda = 0.01
+
+num_epoch = 200
+learning_rate = 0.001
---
+++ preprocessing.py
@@ -0,0 +1,169 @@
+import torch
+import numpy as np
+import scipy.sparse as sp
+
+from collections import Counter
+from sklearn.preprocessing import MultiLabelBinarizer, LabelBinarizer, normalize
+
+def sparse_to_tuple(sparse_mx):
+    if not sp.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+
+def preprocess_graph(adj):
+    adj = sp.coo_matrix(adj)
+    adj_ = adj + sp.eye(adj.shape[0])
+    rowsum = np.array(adj_.sum(1))
+    degree_mat_inv_sqrt = sp.diags(np.power(rowsum, -0.5).flatten())
+    adj_normalized = adj_.dot(degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt).tocoo()
+    return sparse_to_tuple(adj_normalized)
+
+def mask_test_edges(adj):
+    # Function to build test set with 10% positive links
+    # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
+    # TODO: Clean up.
+
+    # Remove diagonal elements
+    adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
+    adj.eliminate_zeros()
+    # Check that diag is zero
+    assert np.diag(adj.todense()).sum() == 0
+
+    # Extract the upper triangular part of the adjacency matrix
+    adj_triu = sp.triu(adj)
+    adj_tuple = sparse_to_tuple(adj_triu)
+    edges = adj_tuple[0]
+    edges_all = sparse_to_tuple(adj)[0]
+    num_test = int(np.floor(edges.shape[0] / 10.))
+    num_val = int(np.floor(edges.shape[0] / 20.))
+
+    # Shuffle edge indices randomly
+    all_edge_idx = list(range(edges.shape[0]))
+    np.random.shuffle(all_edge_idx)
+    val_edge_idx = all_edge_idx[:num_val]
+    test_edge_idx = all_edge_idx[num_val:(num_val + num_test)]
+    test_edges = edges[test_edge_idx]
+    val_edges = edges[val_edge_idx]
+    train_edges = np.delete(edges, np.hstack([test_edge_idx, val_edge_idx]), axis=0)
+
+    def ismember(a, b, tol=5):
+        rows_close = np.all(np.round(a - b[:, None], tol) == 0, axis=-1)
+        return np.any(rows_close)
+
+    # Generate negative samples for the test set
+    test_edges_false = []
+    test_edges_false_set = set(map(tuple, test_edges_false))
+
+    while len(test_edges_false) < len(test_edges):
+        idx_i = np.random.randint(0, adj.shape[0])
+        idx_j = np.random.randint(0, adj.shape[0])
+        if idx_i == idx_j:
+            continue
+        if ismember([idx_i, idx_j], edges_all):
+            continue
+        if (idx_j, idx_i) in test_edges_false_set or (idx_i, idx_j) in test_edges_false_set:
+            continue
+        test_edges_false.append([idx_i, idx_j])
+        test_edges_false_set.add((idx_i, idx_j))
+
+    # Generate negative samples for the validation set
+    val_edges_false = []
+
+    val_edges_false_set = set(map(tuple, val_edges_false))
+    while len(val_edges_false) < len(val_edges):
+        idx_i = np.random.randint(0, adj.shape[0])
+        idx_j = np.random.randint(0, adj.shape[0])
+        if idx_i == idx_j:
+            continue
+        if ismember([idx_i, idx_j], train_edges):
+            continue
+        if ismember([idx_j, idx_i], train_edges):
+            continue
+        if ismember([idx_i, idx_j], val_edges):
+            continue
+        if ismember([idx_j, idx_i], val_edges):
+            continue
+        if (idx_j, idx_i) in val_edges_false_set or (idx_i, idx_j) in val_edges_false_set:
+            continue
+        val_edges_false.append([idx_i, idx_j])
+        val_edges_false_set.add((idx_i, idx_j))
+
+    # Ensure negative samples are not in the original edge set
+    assert ~ismember(test_edges_false, edges_all)
+    assert ~ismember(val_edges_false, edges_all)
+    assert ~ismember(val_edges, train_edges)
+    assert ~ismember(test_edges, train_edges)
+    assert ~ismember(val_edges, test_edges)
+
+    data = np.ones(train_edges.shape[0])
+
+    # Re-build adj matrix
+    adj_train = sp.csr_matrix((data, (train_edges[:, 0], train_edges[:, 1])), shape=adj.shape)
+    adj_train = adj_train + adj_train.T
+
+    # NOTE: these edge lists only contain single direction of edge!
+    return adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false
+
+def eliminate_self_loops(A):
+    """Remove self-loops from the adjacency matrix."""
+    A = A.tolil()
+    A.setdiag(0)
+    A = A.tocsr()
+    A.eliminate_zeros()
+    return A
+
+def largest_connected_components(sparse_graph, n_components=1):
+    _, component_indices = sp.csgraph.connected_components(sparse_graph.adj_matrix)
+    component_sizes = np.bincount(component_indices)
+    components_to_keep = np.argsort(component_sizes)[::-1][:n_components]  # reverse order to sort descending
+    nodes_to_keep = [
+        idx for (idx, component) in enumerate(component_indices) if component in components_to_keep
+    ]
+    return create_subgraph(sparse_graph, nodes_to_keep=nodes_to_keep)
+
+
+def create_subgraph(sparse_graph, _sentinel=None, nodes_to_remove=None, nodes_to_keep=None):
+    # Check that arguments are passed correctly
+    if _sentinel is not None:
+        raise ValueError("Only call `create_subgraph` with named arguments',"
+                         " (nodes_to_remove=...) or (nodes_to_keep=...)")
+    if nodes_to_remove is None and nodes_to_keep is None:
+        raise ValueError("Either nodes_to_remove or nodes_to_keep must be provided.")
+    elif nodes_to_remove is not None and nodes_to_keep is not None:
+        raise ValueError("Only one of nodes_to_remove or nodes_to_keep must be provided.")
+    elif nodes_to_remove is not None:
+        nodes_to_keep = [i for i in range(sparse_graph.num_nodes()) if i not in nodes_to_remove]
+    elif nodes_to_keep is not None:
+        nodes_to_keep = sorted(nodes_to_keep)
+    else:
+        raise RuntimeError("This should never happen.")
+
+    sparse_graph.adj_matrix = sparse_graph.adj_matrix[nodes_to_keep][:, nodes_to_keep]
+    if sparse_graph.attr_matrix is not None:
+        sparse_graph.attr_matrix = sparse_graph.attr_matrix[nodes_to_keep]
+    if sparse_graph.labels is not None:
+        sparse_graph.labels = sparse_graph.labels[nodes_to_keep]
+    if sparse_graph.node_names is not None:
+        sparse_graph.node_names = sparse_graph.node_names[nodes_to_keep]
+    return sparse_graph
+
+
+def binarize_labels(labels, sparse_output=False, return_classes=False):
+    if hasattr(labels[0], '__iter__'):  # labels[0] is iterable <=> multilabel format
+        binarizer = MultiLabelBinarizer(sparse_output=sparse_output)
+    else:
+        binarizer = LabelBinarizer(sparse_output=sparse_output)
+    label_matrix = binarizer.fit_transform(labels).astype(np.float32)
+    return (label_matrix, binarizer.classes_) if return_classes else label_matrix
+
+
+def remove_underrepresented_classes(g, train_examples_per_class, val_examples_per_class):
+    min_examples_per_class = train_examples_per_class + val_examples_per_class
+    examples_counter = Counter(g.labels)
+    keep_classes = set(class_ for class_, count in examples_counter.items() if count > min_examples_per_class)
+    keep_indices = [i for i in range(len(g.labels)) if g.labels[i] in keep_classes]
+
+    return create_subgraph(g, nodes_to_keep=keep_indices)
